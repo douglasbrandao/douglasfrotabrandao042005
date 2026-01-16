6e04308572d29189ffa9d22c29dfdafbd92ceeb6
Douglas Frota Brandao <douglasfrb@192.168.100.27>
2026-01-16 14:02:47 -0400

    feat: adiciona header fixo

---
+++ src/app/layout/header.component.ts
@@ -0,0 +1,19 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  public authService = inject(AuthService);
+  private router = inject(Router);
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
