1036c1403e5210a940d3b75864aac9013c328f94
Douglas Frota Brandao <douglasfrb@gmail.com>
2026-01-16 21:20:29 -0400

    feat: cria componente modal reutilizável

---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent {
+  @Input() open = false;
+  @Input() width = 'max-w-md';
+  @Output() close = new EventEmitter<void>();
+}
