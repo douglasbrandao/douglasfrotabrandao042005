ed2b721c297aeaca1262cf4d8179532d5c125fab
Douglas Frota Brandao <douglasfrb@gmail.com>
2026-01-30 14:22:25 -0400

    feat: implementa burger menu para navegação em mobile devices

--- src/app/layout/header.component.ts
+++ src/app/layout/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
@@ -13,16 +13,25 @@ export class HeaderComponent {
   public authService = inject(AuthService);
   private router = inject(Router);
 
+  menuOpen = signal(false);
+
+  closeMenu() {
+    this.menuOpen.set(false);
+  }
+
   logout() {
+    this.closeMenu();
     this.authService.logout();
     this.router.navigate(['/login']);
   }
 
   goToCreatePet() {
+    this.closeMenu();
     this.router.navigate(['/pet-create']);
   }
 
   goToCreateTutor() {
+    this.closeMenu();
     this.router.navigate(['/tutor-create']);
   }
 }
